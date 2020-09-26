85882dbdbe9f1b6d51b1b1865c4985ea6c464808
Mazupy <70380402+Mazupy@users.noreply.github.com>
2020-09-26 10:37:26 +0200

    updated naming from example

--- src/main/java/com/mazupy/kairyou/mixin/InitMixin.java
+++ src/main/java/com/mazupy/kairyou/mixin/InitMixin.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example.mixin;
+package com.mazupy.kairyou.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class InitMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
 		
